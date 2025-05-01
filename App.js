f84c3b7c101b11748839bf62ccd8a22a5c20b4f5
gnherrera <ginolouisherrera@gmail.com>
2025-05-01 14:29:43 +0800

    1st commit

---
+++ App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Text } from "react-native";
+
+const App = () => {
+return(
+  <Text>
+  I love Programming! so muchssssss
+  </Text>
+);
+
+
+
+
+
+
+}
+
+export default App;
